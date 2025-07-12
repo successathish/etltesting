83876022f3967eb73080de9536a0e9cc1fc0b0d2
sakasi <successathish89@gmail.com>
2025-07-12 20:01:01 +0530

    Testcases added

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-print("hello world")
+import pandas as pd
+import pytest
+
+def test_CheckDuplicates():
+    a=pd.read_csv("target.csv",sep=",")
+    count=a.duplicated().sum()
+    assert count == 0 , "Duplicate rows found"
